18660ba9aea8b88502bda1b9f8fdc9c211a1182f
Jasterix <ljcalixte@gmail.com>
2020-04-16 08:58:58 -0400

    4-baseElement-jquery error

--- 4-javascript-es6-object-oriented-programming-m4-exercise-files/v04/before/src/app.js
+++ 4-javascript-es6-object-oriented-programming-m4-exercise-files/v04/before/src/app.js
@@ -1,20 +1,11 @@
+import $ from "jquery";
 import { Car } from "./classes/car.js";
 import { Drone } from "./classes/drone.js";
 import { fleet } from "./fleet-data.js";
 import { FleetDataService } from "./services/fleet-data-service.js";
+import { Button } from "./ui/button.js";
 
-let dataService = new FleetDataService();
-dataService.loadData(fleet);
+let b = new Button("Click Me");
 
-let car = dataService.getCarByLicense("AT2000");
-// console.log(car);
-
-// let cars = dataService.sortCarsByLicense();
-// console.log(cars);
-
-let cars = dataService.filterCarsByMake("e");
-console.log(cars);
-
-for (let car of cars) {
-  console.log(car.make);
-}
+b.appendToElement($("body"));
+console.log("hello");
