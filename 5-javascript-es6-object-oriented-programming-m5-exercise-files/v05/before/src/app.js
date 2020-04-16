10333cf54231c29b49686ddabc1c004c68602fe2
Jasterix <ljcalixte@gmail.com>
2020-04-16 10:18:24 -0400

    5-5 add image

--- 5-javascript-es6-object-oriented-programming-m5-exercise-files/v05/before/src/app.js
+++ 5-javascript-es6-object-oriented-programming-m5-exercise-files/v05/before/src/app.js
@@ -4,7 +4,10 @@ import { Drone } from "./classes/drone.js";
 import { fleet } from "./fleet-data.js";
 import { FleetDataService } from "./services/fleet-data-service.js";
 import { Button } from "./ui/button.js";
+import { Image } from "./ui/image.js";
 
 let b = new Button("Click Me");
 b.appendToElement($("body"));
-console.log(b);
+
+let i = new Image("../images/drone.jpg");
+i.appendToElement($("body"));
