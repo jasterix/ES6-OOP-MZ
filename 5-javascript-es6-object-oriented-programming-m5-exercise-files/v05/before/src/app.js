4b9b7bdf7b59c8d7073dfcef2df51f0905c9d8dd
Jasterix <ljcalixte@gmail.com>
2020-04-16 09:31:59 -0400

    5-5 button component

--- 5-javascript-es6-object-oriented-programming-m5-exercise-files/v05/before/src/app.js
+++ 5-javascript-es6-object-oriented-programming-m5-exercise-files/v05/before/src/app.js
@@ -1,16 +1,10 @@
-import {Car} from './classes/car.js';
-import {Drone} from './classes/drone.js';
-import {fleet} from './fleet-data.js';
-import {FleetDataService} from './services/fleet-data-service.js';
+import $ from "jquery";
+import { Car } from "./classes/car.js";
+import { Drone } from "./classes/drone.js";
+import { fleet } from "./fleet-data.js";
+import { FleetDataService } from "./services/fleet-data-service.js";
+import { Button } from "./ui/button.js";
 
-let dataService = new FleetDataService();
-dataService.loadData(fleet);
-
-//let car = dataService.getCarByLicense('AT9900');
-
-//let cars = dataService.getCarsSortedByLicense();
-
-let cars = dataService.filterCarsByMake('e');
-
-for (let car of cars)
-    console.log(car.make);
+let b = new Button("Click Me");
+b.appendToElement($("body"));
+console.log(b);
