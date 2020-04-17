7215d2b0aa7f9dd6caae6e889ff27ba20f79a408
Jasterix <ljcalixte@gmail.com>
2020-04-17 08:22:37 -0400

    5-05 data table

--- 5-javascript-es6-object-oriented-programming-m5-exercise-files/v05/before/src/app.js
+++ 5-javascript-es6-object-oriented-programming-m5-exercise-files/v05/before/src/app.js
@@ -6,6 +6,7 @@ import { FleetDataService } from "./services/fleet-data-service.js";
 import { Button } from "./ui/button.js";
 import { Image } from "./ui/image.js";
 import { TitleBar } from "./ui/title-bar.js";
+import { DataTable } from "./ui/data-table.js";
 
 let tb = new TitleBar("Drones and Cars");
 tb.addLink("Home", "");
@@ -13,8 +14,15 @@ tb.addLink("Drones", "");
 tb.addLink("Cars", "");
 tb.appendToElement($("body"));
 
-let b = new Button("Click Me");
-b.appendToElement($("body"));
+// let b = new Button("Click Me");
+// b.appendToElement($("body"));
 
-let i = new Image("../images/drone.jpg");
-i.appendToElement($("body"));
+// let i = new Image("../images/drone.jpg");
+// i.appendToElement($("body"));
+
+let headers = "License Make Model Miles".split(" ");
+let dataService = new FleetDataService();
+dataService.loadData(fleet);
+let dt = new DataTable(headers, dataService.cars);
+
+dt.appendToElement($("body"));
